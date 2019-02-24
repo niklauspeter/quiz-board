6c306b0df67b65fd14674929fb3f5a2f830f4fa0
klaus <oriokiklaus@gmail.com>
2019-02-24 15:16:33 +0300

    add the links of the questions to their respective js Query front and backend commands

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,10 @@ $(document).ready(function(){
   $("#submit").click(function(){
   var answer1= $("input[name='question1']:checked").val();
   var answer2= $("input[name='question2']:checked").val();
-  var answer2= $("input[name='question2']:checked").val();
-  var answer2= $("input[name='question2']:checked").val();
-  var answer2= $("input[name='question2']:checked").val();
+  var answer3= $("input[name='question3']:checked").val();
+  var answer4= $("input[name='question4']:checked").val();
+  var answer5= $("input[name='question5']:checked").val();
+  var answer6= $("input[name='question6']:checked").val();
   var result = 0;
 
 if (answer1== "true")
@@ -23,16 +24,16 @@ if (answer1== "true")
 if (answer2=="true"){
   result = result +20;
 }
-if (answer2=="true"){
+if (answer3=="true"){
   result = result +20;
 }
-if (answer2=="true"){
+if (answer4=="true"){
   result = result +20;
 }
-if (answer2=="true"){
+if (answer5=="true"){
   result = result +20;
 }
-if (answer2=="true"){
+if (answer6=="true"){
   result = result +20;
 }
 alert(result);
