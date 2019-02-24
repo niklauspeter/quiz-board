1abe92b6278c748df55a32f6c3a1f696901af51a
klaus <oriokiklaus@gmail.com>
2019-02-24 14:42:17 +0300

    add jquery command to collect value from the questions in the radio form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
+
+
+
 $(document).ready(function(){
   $(".pointer").click(function(){
     $(".hiddenQuiz").show();
   });
+});
+$(document).ready(function(){
+  alert("run");
+  $("#submit").click(function(){
+  var answer1= $("input[name='question1']:checked").val();
+  var answer2= $("input[name='question2']:checked").val();
+  var answer2= $("input[name='question2']:checked").val();
+  var answer2= $("input[name='question2']:checked").val();
+  var answer2= $("input[name='question2']:checked").val();
+  var result = 0;
+
+  if (answer1== "true")
+{
+  result = result+ 20;
+}
+if (answer2=="true"){
+  result = result +20;
+}
+alert(result);
+});
 });
